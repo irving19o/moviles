7f9894f58e72a54556e7310387f40e6ccaff3cc7
Irving Luna Ramírez <irving19094dev@gmail.com>
2017-11-08 14:03:03 -0600

    final final

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PIL import Image, ImageDraw, ImageFont
 from flask import Flask, send_file, request, jsonify
 from flask_restful import reqparse, abort, Api, Resource, url_for
 import socket
+import random
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,8 +18,10 @@ class Movimiento(Resource):
         derecha = request.args.get('derecha')
         izquierda = request.args.get('izquierda')
         atras = request.args.get('atras')
-        
-        return {"avazar": "frente", "distancia": 50}
+        foo =["izquerda", "derecha", "frente"]
+        return {"avazar": {random.choice(foo): "50"},
+                "imagen": "https://pruebamoodle.ga/derecha="+derecha+"&izquierda="+izquierda+"&frente="+frente+"&atras=100"
+    }
 
 
 api.add_resource(Movimiento, '/movimiento')
